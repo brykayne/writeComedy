7fd8ce330097cbf9034f83eee0fd75105560cd67
Bryan Kayne <brykayne@gmail.com>
2017-05-15 21:36:32 -0400

    Fixed Topics router using Sammy.js. Topics now appear onload of Topics view.

--- app.js
+++ app.js
@@ -4,30 +4,29 @@
 
   var app = $.sammy('#app', function() {
     this.use('Template');
-    this.use('Handlebars', 'hb');
+    
+    $('#app').on('click', '.saveBtn', function(event) {
+      event.preventDefault();
+      console.log('it worked yo');
+    });
 
     this.around(function(callback) {
       var context = this;
       this.load('data/topics.json').then(function(topics) {
         context.topics = topics;
-        console.log(context.topics);
       }).then(callback);
     });
-//Need to take what's in topic.template and put it into a larger topics view which displays in nice format with columns.
+
+    //topics view
     this.get('#/', function(context) {
       context.app.swap('');
 
-      context.render('templates/topic-view.template', context.topics)
+      context.render('templates/topics-view.template').appendTo("#app");
 
-      // $.each(context.topics, function(i, topic) {
-      //   context.render('templates/topic.template', {id: i, topic: topic})
-      //     .appendTo(context.$element());
+      $.each(context.topics, function(i, topic) {
+        context.render('templates/topic.template', {id: i, topic: topic})
+          .appendTo('#topics');
       });
-
-    this.get('#/topic/:id', function(context) {
-      this.topic = this.topics[this.params['id']];
-      if (!this.item) { return this.notFound(); }
-      this.partial('templates/topics-view.template');
     });
 
     this.before('.*', function() {
@@ -35,8 +34,8 @@
       $('nav').find('a').removeClass('current');
       $("nav").find("a[href='"+hash+"']").addClass("current");
     });
-  });
 
+  });
 
   $(function() {
     app.run('#/');
